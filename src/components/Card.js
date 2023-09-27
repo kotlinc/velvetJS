2bb6fa0326d88a29e1f7dcf4926cc36b981efbd9
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-27 10:58:55 +1000

    no duplicatetetetetetetetts

--- src/components/Card.js
+++ src/components/Card.js
@@ -72,35 +72,5 @@ class Card extends HTMLElement {
         })
     }
 }
-const $id = (id) => {
-    const el = document.getElementById(id);
-    return el != null ? el : void 0;
-};
-const $sl = (sl) => {
-    const el = document.querySelectorAll(sl);
-    return el != null ? el : void 0;
-};
-HTMLElement.prototype.text = function(t) {
-    if (t == null) {
-        return this.textContent || void 0;
-    } else {
-        return this != null ? this.textContent = t : void 0;
-    }
-};
-HTMLElement.prototype.html = function(t) {
-    if (t == null) {
-        return this != null ? this.innerHTML : void 0;
-    } else {
-        return this != null ? this.innerHTML = t : void 0;
-    }
-};
-HTMLElement.prototype.val = function(t) {
-    const el = this;
-    if (t == null) {
-        return this != null ? el.value : void 0;
-    } else {
-        return this != null ? el.value = t : void 0;
-    }
-};
 
 define('velvet-card', Card);
